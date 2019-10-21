fccd08ced7dcd9c2da974fd54fa7ae69ec213da5
xiaochi <cumt.xiaochi@gmail.com>
2019-10-21 11:34:48 +0800

    v

--- vector.h
+++ vector.h
@@ -1,6 +1,8 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+#include <stdbool.h>
+
 #include "gc.h"
 
 typedef struct array
@@ -9,47 +11,67 @@ typedef struct array
     void *base;
     int cap;
     int elemSize;
-} Array;
+} array;
 typedef struct vector
 {
     GCFlag flag;
-    Array *array;
+    array *array;
     void *start;
     int len;
     int elemSize;
-} Vector;
+} vector;
 
-#define MakeArray(t, len) _makeArray(len, sizeof(t))
+#define makeArray(t, len) _makeArray(len, sizeof(t))
 
-#define MakeVector(t, len) MakeVectorCap(t, len, len)
-#define MakeVectorCap(t, len, cap) _MakeVectorCap(sizeof(t),len,cap)
+#define makeVector(t, len) makeVectorCap(t, len, len)
+#define makeVectorCap(t, len, cap) _makeVectorCap(sizeof(t), len, cap)
 
-
-#define push(v, e)                                            \
-    do                                                        \
-    {                                                         \
-        Array *arr = v->array;                                 \
-        char *p = v->start;                                   \
-        char *end = p + arr->elemSize * len(v);               \
-        if (end > (char *)arr->data + cap(v) * arr->elemSize) \
-            _vectorEnlarge(v, cap(v) + 1);                    \
-        v->start[v->len] = e;                                 \
-        v->len++;                                             \
+#define push(v, a)      \
+    do                  \
+    {                   \
+        void *p = &(a); \
+        _push(v, a, p); \
+    } while (0)
+#define push_(v, a, p) _Generic(a,                         \
+                                primitiveDef( _push(v, p, sizeof(*p))) \
+                                  float                    \
+                                : _push(v, p, sizeof(*p)), \
+                                  byte                     \
+                                : _push(v, p, sizeof(*p)), \
+                                  bool                     \
+                                : _push(v, p, sizeof(*p)), \
+                                  default                  \
+                                : _push(v, P(*p), sizeof(*p)))
+#define pop(v, a)               \
+    do                          \
+    {                           \
+        void *p = &(a);         \
+        _pop(v, p, sizeof(*p)); \
+    } while (0)
+#define _primitiveDef(t, ...) \
+    t:                        \
+    __VA_ARGS__
+#define primitiveDef(...) _primitiveDef(int, __VA_ARGS__),   \
+                          _primitiveDef(float, __VA_ARGS__), \
+                          _primitiveDef(byte, __VA_ARGS__),  \
+                          _primitiveDef(bool, __VA_ARGS__),
+#define VectorGet(v, i, a)               \
+    do                                   \
+    {                                    \
+        void *p = &(a);                  \
+        _VectorGet(v, i, p, sizeof(*p)); \
+    } while (0)
+#define VectorSet(v, i, a)               \
+    do                                   \
+    {                                    \
+        void *p = &(a);                  \
+        P(a);                            \
+        _VectorSet(v, i, p, sizeof(*p)); \
     } while (0)
 
-#define _defineVectorGet(t)           \
-    t _vectorGet##t(vector *v, int i) \
-    {                                 \
-        return ((t *)v->start)[i];      \
-    }
-_defineVectorGet(int)
-#define vectorGet(v,i) _Generic(t,int:_vectorGetint) (v,t)
-#define vectorSet(v,)
-
-
-int len(vector *v);
+int lenVector(vector *v);
 int cap(vector *v);
-int copy(vector *dst, vector *src);
-vector *vectorSlice(vector *v, int start, int end);
+vector *VectorCopy(vector *v);
+vector *VectorSlice(vector *v, int start, int end);
 
 #endif
