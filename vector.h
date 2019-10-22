7d593aa205d6cb65bd92a6722dc244472a9a728e
xiaochi <cumt.xiaochi@gmail.com>
2019-10-22 23:08:12 +0800

    a

--- vector.h
+++ vector.h
@@ -1,5 +1,5 @@
-#ifndef VECTOR_H
-#define VECTOR_H
+#ifndef _SLICE_H_
+#define _SLICE_H_
 
 #include <stdbool.h>
 
@@ -11,73 +11,75 @@ typedef struct array
     char *type;
     int cap;
     int elemSize;
-    void *base;
+    char *base;
 } array;
-typedef struct vector
+typedef struct slice
 {
     GCFlag flag;
-    char *type;
     array *array;
     int len;
-    int elemSize;
-    void *start;
-} vector;
+    char *start;
+} slice;
 
+/*
+ * there are two ways to check type, #t and _Generic
+ * former is first try, then _Generic
+ * types are checked dynamically
+ */
 #define _sizeof(t) (isPrimitive(#t) ? sizeof(t) : sizeof(t *))
 
 #define _makearray(t, len) _makearray(#t, alloc(array), len, _sizeof(t))
 
-#define _makevector(t, len) _makeVectorCap(makearray(t, cap), #t, _sizeof(t), len, cap)
+#define _makeslice(t, len) _makesliceCap(makearray(t, cap), #t, _sizeof(t), len, cap)
 
-#define push(v, a) _push(v,a)
+#define push(v, a) _push(v, a)
 
-#define _getvector(t, v, i) _vector##t(#t, v, i, a, _sizeof##t())
-#define defVectorGetPri(t)                                            \
-    t _vectorGet##t(char *typeStr, vector##t *v, int i, int elemSize) \
-    {                                                                 \
-        if (strcmp(typeStr, #t) != 0)                                 \
-            error("type error %d %d when vector get", typeStr, #t);   \
-        t a;                                                          \
-        _vectorGet(v, i, &a, elemSize);                               \
-        return a;                                                     \
-    }
-        defVectorGetPri(int)
-#define defVectorGet(t)                                                \
-    t *_vectorGet##t(char *typeStr, vector##t *v, int i, int elemSize) \
-    {                                                                  \
-        if (strcmp(typeStr, #t) != 0)                                  \
-            error("type error %d %d when vector get", typeStr, #t);    \
-        t *a;                                                          \
-        _vectorGet(v, i, &a, elemSize);                                \
-        P(a);                                                          \
-        return a;                                                      \
-    }
-
-#define vectorSet(v, i, a)                \
+#define sliceSet(v, i, a)                \
     do                                    \
     {                                     \
         void *p = &(a);                   \
         P(*p);                            \
-        _vectorSet_(v, i, p, sizeof(*p)); \
+        _sliceSet_(v, i, p, sizeof(*p)); \
     } while (0)
-#define _vectorSet_(v, i, a, p) _Generic(a, primitiveDef(_vectorSet(v, i, p, sizeof(a))), \
+#define _sliceSet_(v, i, a, p) _Generic(a, primitiveDef(_sliceSet(v, i, p, sizeof(a))), \
                                          default                                          \
-                                         : _vectorSet(v, i, a, P(*p)))
-            int lenVector(vector *v);
-int cap(vector *v);
-vector *VectorCopy(vector *v);
-vector *VectorSlice(vector *v, int start, int end);
+                                         : _sliceSet(v, i, a, P(*p)))
+int lenslice(slice *v);
+int cap(slice *v);
+slice *sliceCopy(slice *v);
+slice *sliceSlice(slice *v, int start, int end);
 
-#define pop(t, v) _vectorpop##t(#t, v, sizeof(v->base[0]), sizeof(t))
+#define pop(t, v) _slicepop##t(#t, v, sizeof(v->base[0]), sizeof(t))
 
-#define defVectorPop(t)                                                       \
-    t _vectorpop##t(char *typeStr, vector##t *v, int elemSize, int elemSizeT) \
+#define defslicePop(t)                                                       \
+    t _slicepop##t(char *typeStr, slice##t *v, int elemSize, int elemSizeT) \
     {                                                                         \
         if (strcmp(typeStr, #t) != 0)                                         \
             error("type error %d %d when pop", typeStr, #t);                  \
         t aa;                                                                 \
-        _vectorpop((vector *)v, &aa, elemSize, elemSizeT);                    \
+        _slicepop((slice *)v, &aa, elemSize, elemSizeT);                    \
         return aa;                                                            \
     }
-defVectorPop(int)
+#define pop(t) _Generic((t)->start[0], \
+    int:_vectorpopint(v,sizeof(int)), \
+    unsigned:_vectorpopunsigned(v,sizeof(unsigned)), \
+    short:_vectorpopshort(v,sizeof(short)), \
+    long:_vectorpoplong(v,sizeof(long)), \
+    float:_vectorpopfloat(v,sizeof(float)), \
+    double:_vectorpopdouble(v,sizeof(double)), \
+    char:_vectorpopchar(v,sizeof(char)), \
+    byte:_vectorpopbyte(v,sizeof(byte)), \
+    bool:_vectorpopbool(v,sizeof(bool)), \
+    default:_vectorpopdefault(v,sizeof(char*)))
+
+    _defsliceGet(int)
+_defsliceGet(unsigned)
+_defsliceGet(short)
+_defsliceGet(long)
+_defsliceGet(float)
+_defsliceGet(double)
+_defsliceGet(char)
+_defsliceGet(byte)
+_defsliceGet(bool)
+_defsliceGet(default)
 #endif
