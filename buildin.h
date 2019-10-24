4c4bd901cc1c9fa66d429047e2751e425d00e7a7
xiaochi <cumt.xiaochi@gmail.com>
2019-10-24 11:48:21 +0800

    a

--- buildin.h
+++ buildin.h
@@ -1,47 +1,23 @@
 #ifndef BUILD_H
 #define BUILD_H
 
-#include "stringbuffer.h"
+// #include "str.h"
+#include "slice.h"
+#include "map.h"
 
-#define len(v) _Generic((v),                  \
-                        stringbuffer          \
-                        : stringbufferLen(v), \
-                          str                 \
-                        : strLen(v),          \
-                          array               \
-                        : arrayLen(v),        \
-                          slice               \
-                        : sliceLen(v),        \
-                          map                 \
-                        : mapLen(v), )
+#define len(v) ((v)->len)
 
-#define cap(v) _Generic((v),                  \
-                        stringbuffer          \
-                        : stringbufferCap(v), \
-                          str                 \
-                        : sliceCap(v),        \
-                          slice               \
-                        : sliceCap(v), )
+#define make(t, ...) make##t(__VA_ARGS__)
 
-#define copy(dst, src) _Generic((dst),                        \
-                                stringbuffer                  \
-                                : stringbufferCopy(dst, src), \
-                                  str                         \
-                                : sliceCopy(dst, src),        \
-                                  slice                       \
-                                : sliceCopy(dst, src))
-
-#define make(t, ...) _Generic((t),         \
-                              stringbuffer \
-                              : stringbufferMake(__VA_ARGS__))
-
-#define error(...)                                      \
-    do                                                  \
-    {                                                   \
-        fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); \
-        fprintf(stderr, __VA_ARGS__);                   \
-        fprintf(stderr, "\n");                          \
-        abort();                                        \
-    } while (0)
+#define get(c, ...) _Generic(c,                                    \
+                             slice *                               \
+                             : _getslice((slice *)c, __VA_ARGS__), \
+                               map *                               \
+                             : _getmap((map *)c, __VA_ARGS__))
+#define set(c, ...) _Generic(c,                                    \
+                             slice *                               \
+                             : _setslice((slice *)c, __VA_ARGS__), \
+                               map *                               \
+                             : _setmap((map *)c, __VA_ARGS__))
 
 #endif
