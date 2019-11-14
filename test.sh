cd5febc08a9a1eb94c97aae9272d6fcd5ab16335
xiaochi <cumt.xiaochi@gmail.com>
2019-11-14 15:52:17 +0800

    test a file

--- test.sh
+++ test.sh
@@ -1,2 +1,21 @@
-mingw32-make test
-./test
+
+if [ $# -eq 1 ]; then
+    if [ ! -f "$1.c" ]; then
+        echo "no file $1.c"
+    fi
+    if [ ! -f "$1_test.c" ]; then
+        echo "no file $1_test.c"
+    fi
+
+    # gen
+    fname=.test$1.c
+    echo "#include \"test.h\"" > $fname
+    echo "int main(int argc, char *argv[]){" >> $fname
+    grep -P '^void\s+test\w+' "$1_test.c" | sed 'a\;' >> $fname
+    echo "printf(\"OK.\\n\"); }" >> $fname
+
+    # compile and run
+    gcc -std=c11 -o .test$1 $fname && ./.test$1
+else
+    echo "todo all test"
+fi
